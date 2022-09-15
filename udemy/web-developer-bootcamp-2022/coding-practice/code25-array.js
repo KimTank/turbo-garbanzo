72191ec04854f00d658ad5fcd47cdd6dac9a8cf7
Tae-yoon, Kim <sycork@gmail.com>
2022-09-15 11:47:09 +0900

    object까지 달렷

---
+++ udemy/web-developer-bootcamp-2022/coding-practice/code25-array.js
@@ -0,0 +1,5 @@
+/*Lotto Numbers Exercise
+Let's get some practice creating arrays. Please define a variable called lottoNumbers and set it equal to an array containing any 6 numbers.  It's simple, but we've got to start somewhere!*/
+
+// Define lottoNumbers below:
+let lottoNumbers = [1, 3, 5, 34, 40, 45];
